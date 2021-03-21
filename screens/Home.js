08d64b097423b69fd330232fce08452cf4769b60
candytuft <vishnyc3@gmail.com>
2021-03-21 16:09:39 -0400

    Adds header for home page with icons

--- screens/Home.js
+++ screens/Home.js
@@ -10,6 +10,7 @@ import {
 import { TouchableOpacity } from "react-native-gesture-handler";
 
 import { MaterialIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 import { COLORS, images, SIZES, FONTS } from "../constants";
 
 const Home = () => {
@@ -19,11 +20,43 @@ const Home = () => {
         <TouchableOpacity
           style={{
             width: 50,
+            top: 20,
+            height: 30,
             paddingLeft: SIZES.padding * 2,
             justifyContent: "center",
           }}
         >
           <MaterialIcons name="near-me" size={24} color="black" />
+        </TouchableOpacity>
+
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <View
+            style={{
+              top: 10,
+              width: "70%",
+              height: "100%",
+              backgroundColor: COLORS.lightGray3,
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: SIZES.radius,
+            }}
+          >
+            <Text style={{ ...FONTS.h3 }}>Current Location</Text>
+          </View>
+        </View>
+
+        <TouchableOpacity
+          style={{
+            width: 50,
+            height: 30,
+            top: 20,
+            paddingRight: SIZES.padding * 2,
+            justifyContent: "center",
+          }}
+        >
+          <Ionicons name="cart" size={24} color="black" />
         </TouchableOpacity>
       </View>
     );
