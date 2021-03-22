1bb1313ec15bf963b04dfefc380f921cb2828d5b
candytuft <vishnyc3@gmail.com>
2021-03-21 22:25:01 -0400

    Adds React Native Maps with Expo

--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -1,12 +1,31 @@
-import React from "react";
-import { View, Text } from "react-native";
+import { MailComposer } from "expo";
+import React, { Component } from "react";
 
-const OrderDelivery = () => {
-  return (
-    <View>
-      <Text>Order Delivery Page</Text>
-    </View>
-  );
-};
+import { View, Text, Stylesheet } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
-export default OrderDelivery;
+export default class OrderDelivery extends React.Component {
+  render() {
+    return (
+      <MapView
+        style={{ flex: 1 }}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    );
+  }
+}
+
+// const OrderDelivery = () => {
+//   function renderMap() {
+//     <View></View>;
+//   }
+
+//   return <View style={{ flex: 1 }}>{renderMap()}</View>;
+// };
