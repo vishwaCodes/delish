7e451518a7299288c975af9ef3f2f57a3c20753d
candytuft <vishnyc3@gmail.com>
2021-03-24 01:16:02 -0400

    Adds further functionality

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -20,6 +20,7 @@ import { Colors } from "react-native/Libraries/NewAppScreen";
 const Restaurant = ({ route, navigation }) => {
   const scrollX = new Animated.Value(0);
   const [restaurant, setRestaurant] = React.useState(null);
+  const [orderItems, setOrderItems] = React.useState([]);
   const [currentLocation, setCurrentLocation] = React.useState(null);
 
   React.useEffect(() => {
@@ -27,6 +28,39 @@ const Restaurant = ({ route, navigation }) => {
 
     setRestaurant(item);
   });
+
+  function editOrder(action, menuId, price) {
+    if (action == "+") {
+      let orderList = orderItems.slice();
+      let item = orderList.filter((a) => a.menuId == menuId);
+
+      if (item.length > 0) {
+        let newQty = item[0].qty + 1;
+        item[0].qty = newQty;
+        item[0].total = item[0].qty * price;
+      } else {
+        const newItem = {
+          menuId: menuId,
+          qty: 1,
+          price: price,
+          total: price,
+        };
+        orderList.push(newItem);
+      }
+      setOrderItems(orderList);
+    } else {
+    }
+  }
+
+  function getOrderQty(menuId) {
+    let orderItem = orderItems.filter((a) => a.menuId == menuId);
+
+    if (orderItem.length > 0) {
+      return orderItem[0].qty;
+    }
+
+    return 0;
+  }
 
   function renderHeader() {
     return (
@@ -134,7 +168,7 @@ const Restaurant = ({ route, navigation }) => {
                     justifyContent: "center",
                   }}
                 >
-                  <Text>5</Text>
+                  <Text>{getOrderQty(item.menuId)}</Text>
                 </View>
 
                 <TouchableOpacity
@@ -146,6 +180,7 @@ const Restaurant = ({ route, navigation }) => {
                     borderTopRightRadius: 25,
                     borderBottomRightRadius: 25,
                   }}
+                  onPress={() => editOrder("+", item.menuId, item.price)}
                 >
                   <Text>+</Text>
                 </TouchableOpacity>
